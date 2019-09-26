ff7b591033083d7b88eb48554ac5613968607cbf
Deadshot <pankaj.makhija26@gmail.com>
2019-09-26 13:25:45 +0530

    Add setLogo() in AuthActivity.

--- app/src/main/java/com/example/daggerpractise/AuthActivity.java
+++ app/src/main/java/com/example/daggerpractise/AuthActivity.java
@@ -2,8 +2,12 @@ package com.example.daggerpractise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.bumptech.glide.RequestManager;
 
 import javax.inject.Inject;
 
@@ -11,11 +15,27 @@ import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity {
 
+    @Inject
+    Drawable logo;
+
+    @Inject
+    RequestManager requestManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
+        setLogo();
 
+    }
+
+    /**
+     * This function sets the logo in [activity_auth]
+     */
+    private void setLogo(){
+        requestManager
+                .load(logo)
+                .into((ImageView)findViewById(R.id.login_logo));
     }
 }
